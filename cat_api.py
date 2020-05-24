63c5ceb42a870432f80b1406c2d69b2ff6f28884
hayleysuss <54334173+hayleysuss@users.noreply.github.com>
2020-05-24 19:29:12 -0400

    Update cat_api.py

--- cat_api.py
+++ cat_api.py
@@ -23,8 +23,8 @@ def get_dict_of(type):
       dict = {}
       try:
          all_of_type = requests.get('https://api.thecatapi.com/v1/' + type, headers = headers).json()
-      except: 
-         print("Error fetching types")
+      except Exception as e: 
+         print(e)
          exit()
       for i in range (len(all_of_type)):
          dict[all_of_type[i].get('name')] = all_of_type[i].get('id')
@@ -63,8 +63,8 @@ def load_page(p, i):
    payload['page'] = p
    try:
       r = requests.get('https://api.thecatapi.com/v1/images/search', headers = headers, params = payload)
-   except:
-      print("Error fetching next page of images")
+   except Exception as e:
+      print(e)
    array = r.json()
    print(len(array))
 
@@ -74,8 +74,8 @@ def load_page(p, i):
          img_r = requests.get(str(curr.get('url')), headers = headers)
          img = Image.open(BytesIO(img_r.content))
          img.save("%scatphoto%d.jpg" % (path, j + i + 1))
-      except:
-         print("Error fetching images")
+      except Exception as e:         
+         print(e)
    
    #num cats loaded
    return len(array)
@@ -103,10 +103,76 @@ def save_cats():
          page += 1
          i += limit
 
-option = input("Save cat images (1) or Vote on cats (2)")
+def get_random_cat():
+   try:
+      r = requests.get('https://api.thecatapi.com/v1/images/search', headers = headers, params = { 'limit': 1 })
+      curr = r.json()[0]
+      image_id = curr.get('id')
+      img_r = requests.get(str(curr.get('url')), headers = headers)
+      img = Image.open(BytesIO(img_r.content))
+      img.show()
+
+   except Exception as e:
+      print(e)
+      exit()
+
+   return image_id
+
+def favorite_cat(image_id):
+   body = {
+      'image_id': image_id,
+      'sub_id': 'user'
+   }
+   try:
+      r = requests.post('https://api.thecatapi.com/v1/favourites', body, headers = headers, params = payload)
+   except Exception as e:
+      print(e)
+      exit()
+   return r
+
+
+def get_favorites():
+   path = input("Path to the directory where cat photos will be saved (ex. /path/to/my/directory/): ")
+   params = {
+      'limit': '1',
+      'page': '1',
+      'sub_id': 'user'
+   }
+   try:
+      r = requests.get('https://api.thecatapi.com/v1/favourites', headers = headers, params = params)
+      print(r.json())
+      array = r.json()
+      print(len(array))
+      for i in range(len(array)):
+         curr = array[i]
+         print(curr)
+         img_r = requests.get(str(curr.get('url')), headers = headers)
+         img = Image.open(BytesIO(img_r.content))
+         img.save("%sfavorite%d.jpg" % (path, i + 1))
+
+   except Exception as e:
+      print(e)
+      exit()
+
+
+option = input("Search cats (1) or Favorite cats (2)")
 if option == '1':
    save_cats()
+elif option == '2':
+   favorite_or_get = input("Favorite (1) or Get and save favorites? (2)")
+   if favorite_or_get == '1':
+      image_id = get_random_cat()
+      favorite = input('Favorite this cat? YES or NO: ')
+      if favorite == 'YES':
+         favorite_cat(image_id)
+         print("Successfully favorited")
+   elif favorite_or_get == '2':
+      get_favorites()
+   else:
+      print("Not a valid option")
+
 else:
+   print("Not a valid option")
    exit()
 
 
